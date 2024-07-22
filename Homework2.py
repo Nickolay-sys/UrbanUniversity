437da1a1a14f4d7febdb7375c693af3478dc3bd2
Nickolay-sys <QuokkaNikolay@yandex.ru>
2024-07-22 10:58:05 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,10 @@
+the_number_of_completed_hw = 12
+number_of_hours_spent = 1.5
+name_of_course = 'Python'
+time_for_one_task = (number_of_hours_spent/the_number_of_completed_hw )
+print('Курс: '+ str(name_of_course) 
+      + ', всего задач:' + str(the_number_of_completed_hw)
+      + ', затрачано часов:'+ str(number_of_hours_spent)
+      + ', среднее время выполнения ' + str(time_for_one_task) 
+      + ' часа'
+      )
