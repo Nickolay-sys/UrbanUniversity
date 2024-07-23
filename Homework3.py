e2f1500e891672575f681829be1642da73397fbf
Nickolay-sys <QuokkaNikolay@yandex.ru>
2024-07-23 10:21:37 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,12 @@
+name = "Nikolay"
+age = 26
+new_age = age + 1
+is_student = True
+print('Name: '
+      + name)
+print('Age: '
+      + str(age))
+print('New age: '
+      + str(new_age))
+print('Is Student: '
+      + str(is_student))
