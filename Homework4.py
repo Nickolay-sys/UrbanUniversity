9cd278dda390da49dc4c2a0fb50ccabd60a07377
Nickolay-sys <QuokkaNikolay@yandex.ru>
2024-07-24 11:20:48 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,16 @@
+my_string = input()     # Я учусь в Urban University
+print(
+    my_string.upper()
+)
+print(
+    my_string.lower()
+)
+print(
+    my_string.replace(' ', '')
+)
+print(
+    my_string[0]
+)
+print(
+    my_string[-1]
+)
