5a431c020339d5f651de94b16589e9e6469b0f27
Nickolay-sys <QuokkaNikolay@yandex.ru>
2024-08-27 15:08:11 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -2,9 +2,8 @@ the_number_of_completed_hw = 12
 number_of_hours_spent = 1.5
 name_of_course = 'Python'
 time_for_one_task = (number_of_hours_spent/the_number_of_completed_hw )
-print('Курс: '+ str(name_of_course) 
-      + ', всего задач:' + str(the_number_of_completed_hw)
-      + ', затрачано часов:'+ str(number_of_hours_spent)
-      + ', среднее время выполнения ' + str(time_for_one_task) 
-      + ' часа'
+print(f'Курс:  {name_of_course}, ' 
+      f'всего задач: {the_number_of_completed_hw}, '
+      f'затрачано часов: {number_of_hours_spent}, '
+      f'среднее время выполнения  {time_for_one_task} часа, '
       )
