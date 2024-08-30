7c525bd2a815434c89b71dbcf70d5052a37e9907
Nickolay-sys <QuokkaNikolay@yandex.ru>
2024-08-30 10:07:14 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -6,10 +6,12 @@ class House:
         if new_floor <= self.number_of_floors:
             for i in range(new_floor):
                 print(i + 1)
-        if new_floor > self.number_of_floors:
+        if new_floor > self.number_of_floors or new_floor <= 0:
             print(f'Такого этажа не существует')
             
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h1.go_to(0)
+h2.go_to(-10)
